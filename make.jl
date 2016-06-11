528f79a4d619a5d7105a89c52a669a6e0bf2cf67
Josh Day <emailjoshday@gmail.com>
2016-06-11 18:26:38 -0500

    Deployed ed61e9f with MkDocs version: 0.15.3

---
+++ make.jl
@@ -0,0 +1,28 @@
+module MakeOnlineStatsDocs
+reload("OnlineStats")
+using OnlineStats
+
+rootdir = Pkg.dir("OnlineStats")
+
+# Generate api.md
+api = rootdir * "/doc/api.md"
+rm(api)
+touch(api)
+file = open(api, "r+")
+write(file, "<!--- Generated at " * string(now()) * ".  Don't edit --->\n")
+write(file, "# API\n\n")
+info("The following items are included in the output file:\n")
+nms = setdiff(names(OnlineStats), [:OnlineStats])  # Vector{Symbol} of names
+for nm in nms
+    @eval obj = OnlineStats.$nm
+    d = Docs.doc(obj)
+    if d != nothing
+        println(nm)
+        write(file, "## " * string(nm) * "\n" * Markdown.plain(d) * "\n[Top](#api)\n")
+    end
+end
+close(file)
+
+cd(rootdir)
+run(`mkdocs gh-deploy --clean`)
+end  # MakeOnlineStatsDocs
